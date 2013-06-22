a24127e402a9e0ce2802802eafb650c1f88268c0
Shalom Joseph <shalom.i.joseph@gmail.com>
2013-06-22 10:13:55 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Banana::Application.config.secret_token = '9012d3e10fdbe8520437765adcb90a16aba65c8e8e03290e8dbe144303730c95849f14ebdf046c836402f7e42c88eafb2607def74fa89280dcd073d16c7cd317'
